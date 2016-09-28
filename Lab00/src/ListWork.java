07fbae08bd76ee2aa19f6eaff7441eaaca114a05
Lucas Robertson <lucasrobertson0@gmail.com>
2016-09-28 10:58:12 -0700

    added netbeans project dir

--- Lab00/src/ListWork.java
+++ Lab00/src/ListWork.java
@@ -2,17 +2,15 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 /**
  * A class that implements some basic list functionality and helps in testing it
- * 
+ *
  * @author Lucas Robertson
  * @version Lab00
  */
 public class ListWork {
 
     static final int SIZE = 10;
-
 
     public static void main(String[] args) {
         //Initialize
@@ -23,40 +21,38 @@ public class ListWork {
         int i = 0;
         while (i < SIZE) {
             try {
-                sout("Enter an integer (" + (i + 1) + ") : ", false);
+                System.out.print("Enter an integer (" + (i + 1) + ") : ");
                 values[i] = input.nextInt();
                 i++;
             } catch (InputMismatchException ime) {
-                sout("Invalid input!");
+                System.out.println("Invalid input!");
                 input.nextLine();
             }
         }
         input.nextLine();
 
-
         //Prompt the user to seach, and do so if they specify
         while (true) {
-            sout("Type \"y\" to search, or anything else to finish: ", false);
+            System.out.print("Type \"y\" to search, or anything else to finish: ");
             if (input.nextLine().toLowerCase().equals("y")) {
-                sout("Enter a search target: ", false);
+                System.out.print("Enter a search target: ");
                 try {
                     int target = input.nextInt();
-                    if(search(values, target)) {
-                        sout("The array contained the specified value!");
+                    if (search(values, target)) {
+                        System.out.println("The array contained the specified value!");
                     } else {
-                        sout("The array didn't contain that value, sorry :(");
+                        System.out.println("The array didn't contain that value, sorry :(");
                     }
-                    
+
                     input.nextLine();
                 } catch (InputMismatchException ime) {
-                    sout("Input was not an integer!");
+                    System.out.println("Input was not an integer!");
                     input.nextLine();
                 }
             } else {
                 break;
             }
         }
-
 
         print(values);
 
@@ -66,16 +62,27 @@ public class ListWork {
     }
 
     /**
-     * Function that searches through a specified array to see if it contains a certain value
-     * 
-     * @param arr  The array to search through
-     * @param target  The value to search for
-     * @return A boolean value, true if the given array contains the specified value, false otherwise
+     * Function that searches through a specified array to see if it contains a
+     * certain value
+     *
+     * @param arr The array to search through
+     * @param target The value to search for
+     * @return A boolean value, true if the given array contains the specified
+     * value, false otherwise
      */
     public static <T> boolean search(T[] arr, T target) {
+        if (arr == null || arr.length == 0) {
+            
+            return false;
+        }
+
         for (T item : arr) {
-            if (target.equals(item)) {
-                return true;
+            try {
+                if (target.equals(item)) {
+                    return true;
+                }
+            } catch (NullPointerException npe) {
+                return false;
             }
         }
 
@@ -83,34 +90,15 @@ public class ListWork {
     }
 
     /**
-     * A function that prints the values (using toString()) of all of the members of a given array
-     * 
-     * @param arr  The array through which to iterate and print values
+     * A function that prints the values (using toString()) of all of the
+     * members of a given array
+     *
+     * @param arr The array through which to iterate and print values
      */
     public static <T> void print(T[] arr) {
         for (T item : arr) {
-            sout(item.toString());
+            System.out.println(item.toString());
         }
     }
-
-    /**
-     * Quality of life, ignore
-     */
-    public static void sout(String output) {
-        System.out.println(output);
-    }
-
-    /**
-     * ignore
-     */
-    public static void sout(String output, boolean newln) {
-        if (newln) {
-            System.out.println(output);
-        } else {
-            System.out.print(output);
-        }
-
-    }
-
 
 }
---
+++ Lab00/src/Separator.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Lucas Robertson
+ * @version Lab00
+ */
+public class Separator {
+    public static void main(String[] args) {
+        
+    }
+}
