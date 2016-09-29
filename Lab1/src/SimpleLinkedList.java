06075395ab1b38076a6f0d00d967692eaad42946
Lucas Robertson <lucasrobertson0@gmail.com>
2016-09-29 14:42:08 -0700

    work

--- Lab1/src/SimpleLinkedList.java
+++ Lab1/src/SimpleLinkedList.java
@@ -1,36 +1,80 @@
+
 /**
- * 
- * 
+ *
+ *
  * @author Lucas Robertson
  * @version Lab 1
  */
-public class SimpleLinkedList<T> implements SimpleList {
+public class SimpleLinkedList implements SimpleList {
 
     private int size;
-    
+    private Node first;
+    private Node last;
+
+    public SimpleLinkedList() {
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+    }
+
     @Override
     public <T> void add(int index, T element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public <T> void add(T element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (size == 0) {
+            first = new Node(element);
+            last = first;
+        } else {
+            Node newNode = new Node(element);
+
+            newNode.previous = last;
+            last = newNode;
+            last.previous.next = newNode;
+        }
+
+        size++;
+
     }
 
     @Override
     public <T> T get(int index) throws IndexOutOfBoundsException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        int i = 0;
+        Node current = first;
+        while(i < index) {
+            current = current.next;
+        }
+        
+        return current.element;
     }
 
     @Override
     public <T> T remove(int index) throws IndexOutOfBoundsException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.size;
     }
-    
+
+    private class Node<E> {
+
+        public E element;
+        public Node next;
+        public Node previous;
+
+        public Node(E element) {
+            this.element = element;
+            this.next = null;
+            this.previous = null;
+        }
+    }
+
 }
