3b56da477feb76307b253f20e2c5cb4956dbf39d
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-12 23:22:59 -0700

    work

--- Lab3/test/BSCTests.java
+++ Lab3/test/BSCTests.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -10,7 +5,8 @@ import static org.junit.Assert.*;
 /**
  * Tests for the Balanced Symbol Checker
  *
- * @author liblabs-user
+ * @author Lucas Robertson
+ * @version Lab 3
  */
 public class BSCTests {
 
