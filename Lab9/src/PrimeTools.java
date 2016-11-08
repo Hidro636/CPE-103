3778155b64501cff034ef0e986854ec64e1914e3
Lucas Robertson <lucasrobertson0@gmail.com>
2016-11-07 17:54:30 -0800

    work

---
+++ Lab9/src/PrimeTools.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Lucas Robertson
+ * @version Lab 9
+ */
+public class PrimeTools {
+
+    public static boolean isPrime(int value) {
+
+    }
+
+    public static int nextPrime(int value) {
+
+    }
+}
