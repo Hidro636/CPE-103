c96305d9586a656c8b6445b87c08c4a1a8bbaec2
Lucas Robertson <lucasrobertson0@gmail.com>
2016-11-14 13:01:07 -0800

    work

--- Program4/src/MorseToText.java
+++ Program4/src/MorseToText.java
@@ -49,7 +49,7 @@ public class MorseToText implements BSTTranslator<MorseOrder> {
 
     @Override
     public BST<MorseOrder> getBST() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return bst;
     }
 
     @Override
