c5017263336167003fed21b94d384ab459701346
Lucas Robertson <lucasrobertson0@gmail.com>
2016-11-16 10:26:10 -0800

    work

--- Program5/test/HashTableSCTimingTests.java
+++ Program5/test/HashTableSCTimingTests.java
@@ -1,4 +1,5 @@
 
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,10 +18,33 @@ public class HashTableSCTimingTests {
     public HashTableSCTimingTests() {
     }
 
+    private String randomString(int length) {
+        Random r = new Random(9555);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append((char) (r.nextInt(26) + 97));
+        }
+
+        return sb.toString();
+    }
+
     @Test
     public void testAddTime() {
         System.out.println("Timing add()...");
-        
-        
+        HashTableSC instance;
+        long start, end;
+        int[] sizes = new int[]{5000, 10000, 50000, 100000, 1000000, 5000000, 100000000};
+
+        for (int i : sizes) {
+            instance = new HashTableSC(i);
+            start = System.nanoTime();
+            for (int j = 0; j < sizes.length / 2; j++) {
+                instance.add(j);
+            }
+            end = System.nanoTime();
+            System.out.println("\t" + i + " elements: " + (end - start) + "ns");
+        }
     }
+    
+    
 }
