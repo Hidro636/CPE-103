1973ca95773d8f62ce9f06c97fc7ae11501375ce
Lucas Robertson <lucasrobertson0@gmail.com>
2016-11-28 09:13:43 -0800

    work

---
+++ Lab11/src/ShellSorts.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author Lucas Robertson
+ * @version Lab 11
+ */
+public class ShellSorts {
+
+    static <T extends Comparable<? super T>> void heapSort(T[] array) {
+
+    }
+
+    static <T extends Comparable<? super T>> void hubbard(T[] array) {
+
+    }
+
+    static <T extends Comparable<? super T>> void sedgewick(T[] array) {
+
+    }
+
+    static <T extends Comparable<? super T>> void shell(T[] array) {
+
+    }
+
+}
---
+++ Lab11/test/ShellSortsTests.java
@@ -0,0 +1,47 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lucas Robertson
+ * @version Lab 11
+ */
+public class ShellSortsTests {
+
+    public ShellSortsTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of test method, of class ShellSorts.
+     */
+    @Test
+    public void testTest() {
+        System.out.println("test");
+        ShellSorts instance = new ShellSorts();
+        instance.test();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
