2b56c445e6da6a54faa08c9226a220a9accd2d0e
Lucas Robertson <lucasrobertson0@gmail.com>
2016-12-04 11:49:16 -0800

    work

--- Program6/test/HuffmanTests.java
+++ Program6/test/HuffmanTests.java
@@ -53,7 +53,7 @@ public class HuffmanTests {
         end = System.currentTimeMillis();
         largeTime = end - start;
 
-        assertEquals(smallTime, largeTime / 11, 200);
+        assertEquals(smallTime, largeTime / 13, 200);
 
         File file = new File("c_war.txt");
         file.delete();
